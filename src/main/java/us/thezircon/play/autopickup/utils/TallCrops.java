5fa5f4fd83e87cc5d37ac1255b20d5e1a4bed76c
yLeoft <maehara.leonardo@gmail.com>
2025-04-12 16:28:38 -0300

    Fix Kelp, Twisting Vines & Weeping Vines

--- src/main/java/us/thezircon/play/autopickup/utils/TallCrops.java
+++ src/main/java/us/thezircon/play/autopickup/utils/TallCrops.java
@@ -12,39 +12,23 @@ public class TallCrops {
     public ArrayList<Material> verticalReqDown = new ArrayList<>();
 
     public TallCrops () {
-        //verticalReqDown.add(Material.CAVE_VINES_PLANT);
-        //verticalReqDown.add(Material.CAVE_VINES);
-        //verticalReq.add(Material.SUGAR_CANE);
-        //verticalReq.add(Material.CACTUS);
-        //verticalReq.add(Material.KELP);
-        //verticalReq.add(Material.KELP_PLANT);
-
-        if (
+         if (
+                Bukkit.getVersion().contains("1.14") ||
+                Bukkit.getVersion().contains("1.15") ||
+                Bukkit.getVersion().contains("1.16") ||
                 Bukkit.getVersion().contains("1.17") ||
                 Bukkit.getVersion().contains("1.18") ||
                 Bukkit.getVersion().contains("1.19") ||
                 Bukkit.getVersion().contains("1.20") ||
                 Bukkit.getVersion().contains("1.21")
         ) {
-            verticalReqDown.add(Material.WEEPING_VINES_PLANT);
-            verticalReqDown.add(Material.WEEPING_VINES);
-            verticalReq.add(Material.TWISTING_VINES_PLANT);
-            verticalReq.add(Material.TWISTING_VINES);
+            verticalReq.add(Material.KELP);
+            verticalReq.add(Material.KELP_PLANT);
             verticalReq.add(Material.BAMBOO);
             verticalReq.add(Material.BAMBOO_SAPLING);
-        } else if (Bukkit.getVersion().contains("1.16")) {
-            verticalReqDown.add(Material.WEEPING_VINES);
-            verticalReqDown.add(Material.WEEPING_VINES_PLANT);
-            verticalReq.add(Material.TWISTING_VINES_PLANT);
-            verticalReq.add(Material.TWISTING_VINES);
-            verticalReq.add(Material.BAMBOO);
-            verticalReq.add(Material.BAMBOO_SAPLING);
-        } else if (
-                Bukkit.getVersion().contains("1.14") ||
-                Bukkit.getVersion().contains("1.15")
-        ) {
-            verticalReq.add(Material.BAMBOO);
-            verticalReq.add(Material.BAMBOO_SAPLING);
+        }else if(Bukkit.getVersion().contains("1.13")) {
+            verticalReq.add(Material.KELP);
+            verticalReq.add(Material.KELP_PLANT);
         }
     }
 
